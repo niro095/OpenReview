ee90695f2446211f58161dd13a1d2040769c1d07
DN3RO <nirsaias095@gmail.com>
2022-12-13 01:58:58 +0200

    Update main5.py

--- main5.py
+++ main5.py
@@ -3,13 +3,14 @@ import os
 import git
 
 # Set up OpenAI API key
-openai.api_key = "<YOUR_OPENAI_API_KEY>"
+openai_api_key = input("Enter your OpenAI API key: ")
+openai.api_key = openai_api_key
 
 # Set the URL of the GitHub repository to use as input
-github_url = "<URL_OF_GITHUB_REPOSITORY>"
+github_url = input("Enter the URL of the GitHub repository to use as input: ")
 
 # Clone the repository locally
-local_repo_path = "<LOCAL_DIRECTORY_TO_CLONE_REPO>"
+local_repo_path = input("Enter the local directory to clone the repository to: ")
 repo = git.Repo.clone_from(github_url, local_repo_path)
 
 # Use OpenAI to optimize and document the code
@@ -17,7 +18,7 @@ optimized_code = openai.optimize_code(repo.working_tree_dir)
 documented_code = openai.add_documentation(optimized_code)
 
 # Save the optimized and documented code to a new local repository
-output_repo_path = "<LOCAL_DIRECTORY_FOR_OUTPUT_REPO>"
+output_repo_path = input("Enter the local directory for the output repository: ")
 output_repo = git.Repo.init(output_repo_path)
 output_repo.index.add(documented_code)
 output_repo.index.commit("Optimized and documented code with OpenAI")
